033f2377fde8e59aa6c5504ff38e4942370962a4
GitMiku <MikuMiku@lavabit.com>
2013-10-20 22:41:37 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,7 +1,7 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-$url = "http://ltfu.pw/shorten.php?longurl=" . $_POST['url'];
-echo file_get_contents($url);
-}
+    {
+      $url = "http://ltfu.pw/shorten.php?longurl=" . $_POST['url'];
+      echo file_get_contents($url);
+    }
 ?>
